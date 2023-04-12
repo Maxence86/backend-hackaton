f149792f7f9f7a6a279f9b0367c04a193ba890e1
Xiaohuan <cao_xiaohuan@hotmail.fr>
2023-04-12 11:50:09 +0200

    route

--- routes/trips.js
+++ routes/trips.js
@@ -1,26 +1,27 @@
 var express = require('express');
-
+var moment = require('moment')
 var router = express.Router();
 var Trip = require('../models/trips')
 
-// router.get('/', (req, res) => {
+router.get('/', (req, res) => {
 
-//     Trip.find( { 
-//         departure : { $regex : new RegExp(req.body.departure, "i") },
-//         arrival : { $regex : new RegExp(req.body.arrival, "i") },
-//     }
-//     ).then(data => {
-//         data.filter(e => new Date(e.date).getTime() == new Date(req.body.date).getTime())
-//         res.json({ trips: data });
-//     });
-// });
+    Trip.find( { 
+        departure : { $regex : new RegExp(req.body.departure, "i") },
+        arrival : { $regex : new RegExp(req.body.arrival, "i") },
+    }
+    ).then(data => {
+        console.log(moment(req.body.date))
+        let trips = data.filter(e => moment(e.date, 'YYYY-MM-DD').isSame(moment(req.body.date,'YYYY-MM-DD' ), 'day'))
+        res.json({trips});
+    });
+});
 
 
-router.get('/',(req, res) =>
-Trip.find()
-.then(data=>{
-    res.json({data})
-}))
+// router.get('/',(req, res) =>
+// Trip.find()
+// .then(data=>{
+//     res.json({data})
+// }))
 
 
 module.exports = router;
