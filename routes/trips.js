a79d26c166924f4a121605db9b8efb76eace323c
Xiaohuan <cao_xiaohuan@hotmail.fr>
2023-04-12 09:23:16 +0200

    route

--- routes/trips.js
+++ routes/trips.js
@@ -1,16 +1,17 @@
 var express = require('express');
+
 var router = express.Router();
 var Trip = require('../models/trips')
 
-router.get('/trips', (req, res) => {
+router.get('/', (req, res) => {
 
-    Trip.findOne( 
+    Trip.find( {
         
-    
-         
-        // e => e.departure.toLowerCase() === req.body.departure.toLowerCase() &&
-        //     e.arrival.toLowerCase() === req.body.departure.toLowerCase() &&
-        //     new Date(e.date).getTime() === new Date(req.body.date).getTime(),
+        departure : { $regex : new RegExp(req.body.departure, "i") },
+        arrival : { $regex : new RegExp(req.body.arrival, "i") },
+        date : new Date(req.body.date).getTime()
+
+    }
 
     ).then(data => {
         res.json({ trips: data });
