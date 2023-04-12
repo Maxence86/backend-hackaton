f21ed7923698da8ce07a777e2d81606fd2a52bc1
Maxence Dassaud <max.dassaud@hotmail.fr>
2023-04-12 17:28:31 +0200

    cart

--- models/cart.js
+++ models/cart.js
@@ -4,7 +4,7 @@ const cartSchema = mongoose.Schema({
     departure : String,
     arrival: String,
     date: String,
-    price: Number    
+    price: Number,
 });
 
 const Cart = mongoose.model('carts', cartSchema);
