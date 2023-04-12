0039d8ed175f59b4b5df8651270e2a39c29758f4
Maxence Dassaud <max.dassaud@hotmail.fr>
2023-04-12 11:51:35 +0200

    test

--- routes/trips.js
+++ routes/trips.js
@@ -3,24 +3,24 @@ var express = require('express');
 var router = express.Router();
 var Trip = require('../models/trips')
 
-// router.get('/', (req, res) => {
+router.get('/', (req, res) => {
 
-//     Trip.find( { 
-//         departure : { $regex : new RegExp(req.body.departure, "i") },
-//         arrival : { $regex : new RegExp(req.body.arrival, "i") },
-//     }
-//     ).then(data => {
-//         data.filter(e => new Date(e.date).getTime() == new Date(req.body.date).getTime())
-//         res.json({ trips: data });
-//     });
-// });
+    Trip.find( { 
+        departure : { $regex : new RegExp(req.body.departure, "i") },
+        arrival : { $regex : new RegExp(req.body.arrival, "i") },
+    }
+    ).then(data => {
+        data.filter(e => new Date(e.date).getTime() == new Date(req.body.date).getTime())
+        res.json({ trips: data });
+    });
+});
 
 
-router.get('/',(req, res) =>
-Trip.find()
-.then(data=>{
-    res.json({data})
-}))
+// router.get('/',(req, res) =>
+// Trip.find()
+// .then(data=>{
+//     res.json({data})
+// }))
 
 
 module.exports = router;
